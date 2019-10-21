d3b4edd3656dbcfb8041ef0200b83d3014682b5b
dlqudwns <dlqudwns2002@gmail.com>
2019-10-21 15:41:43 +0900

    modified condor.sh

--- condor.sh
+++ condor.sh
@@ -2,4 +2,4 @@
 source /home/bjlee/.bashrc
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/bjlee/.mujoco/mujoco200/bin
 cd /home/bjlee/PycharmProjects/RepBM
-/home/bjlee/anaconda3/envs/MBRLnets/bin/python main_pendulum.py --pid=$1
+/home/bjlee/anaconda3/envs/MBRLnets/bin/python main_cont.py --pid=$1
